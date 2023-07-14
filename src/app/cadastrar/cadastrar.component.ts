2e0a8a9cce0f816236382467c656c2c15445d6fd
Carol Trindade <caroline.alvestrindade@hotmail.com>
2023-07-14 05:22:40 -0300

    feat: campos do cadastro: implementando

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-cadastrar',
   templateUrl: './cadastrar.component.html',
   styleUrls: ['./cadastrar.component.css']
 })
+
 export class CadastrarComponent {
 
+  hab: Array<string> = [];
+
+  selectedHab!: string;
+
+  selectHab(hab: string) {
+  this.selectedHab = hab; 
+}
+
+@ViewChildren('habs') itemsElements!: QueryList<ElementRef>;
+
+  habs = [
+    { label: 'Item 1', isSelected: false },
+    { label: 'Item 2', isSelected: false },
+    { label: 'Item 3', isSelected: false },
+    // ... outros itens
+  ];
+
+  toggleHab(hab: any) {
+    hab.isSelected = !hab.isSelected;
+  }
 }
